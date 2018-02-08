d31d216af5a6e91e318bec1c4d063d185b77d8b7
Karthik Kasturi <karthik.kasturi@GGKU4MPC62>
2018-02-08 18:15:11 +0530

    Start Teracking method added

--- Structure/UntrackedRepository.cs
+++ Structure/UntrackedRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,9 +31,34 @@ namespace Structure
             CreatedDate = createdDate;
         }
 
-        //public TrackedRepository StartTracking()
-        //{
-        //    return new TrackedRepository();
-        //}
+        public TrackedRepository StartTracking()
+        {
+            string url = GetRemoteURL();
+            TrackedRepository repo = new TrackedRepository()
+                { 
+                    IsActive = true,
+                    WorkingDirectory = this.WorkingDirectory,
+                    CreatedDate = this.CreatedDate,
+                    RemoteURL = url
+                };
+            repo.Refresh();
+            return repo;
+        }
+
+        private string GetRemoteURL()
+        {
+            string[] config = File.ReadAllText(WorkingDirectory + @"\.git\config").Split('\n');
+            
+            for (int i = 0; i < config.Length; i++)
+            {
+                if (config[i].Contains("url = "))
+                {
+                    string[] splitLine = config[i].Split(' ');
+                    return splitLine[splitLine.Length - 1];
+                }
+            }
+            return null;
+            
+        }
     }
 }
